85be0b724287990c00658675a1c0e2d148af8790
EC2 Default User <ec2-user@ip-172-31-23-128.ap-northeast-1.compute.internal>
2023-07-28 07:52:28 +0000

    create task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,42 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
+                <form method="POST" action="{{ route('admin.profile.create') }}">
+                    
+                    @if (count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <div class="form-group row">
+	                    <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+	                    <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="checkbox" class="form-control" name="gender">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+	                    <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+	                    <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction"></textarea> 
+                        </div>
+                    </div>
+                    @csrf
+                    <input type="submit" class="btn btn-light" value="決定">
+                </form>
             </div>
         </div>
     </div>
