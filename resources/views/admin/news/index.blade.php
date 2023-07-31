e8babf3f1aa8ce305854991e851b510e261542a7
EC2 Default User <ec2-user@ip-172-31-23-128.ap-northeast-1.compute.internal>
2023-07-31 08:45:40 +0000

    create edit and update

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -45,6 +45,12 @@
                                 <th>{{ $news->id }}</th>
                                 <td>{{ Str::limit($news->title, 100) }}</td>
                                 <td>{{ Str::limit($news->body, 250) }}</td>
+                                <td>
+                                    <div>
+                                        <a href="{{ route('admin.news.edit', ['id' => $news->id]) }}">
+                                        編集</a>
+                                    </div>
+                                </td>
                             </tr>
                             @endforeach
                         </tbody>
